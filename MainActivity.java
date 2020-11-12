ccb466008988be8000246004990f733b68fa3b8f
athegreat8 <60903042+athegreat8@users.noreply.github.com>
2020-11-11 16:08:52 -0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,31 +1,33 @@
-package com.example.practice3;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
 
     Button AddMealButton;
+    Button CreateMealButton;
+    Button MealOptionButton;
+    Button HistoryButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    { super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AddMealButton = (Button)findViewById(R.id.AddMealButton);
+        CreateMealButton = (Button)findViewById(R.id.CreateMealButton);
+        MealOptionButton= (Button)findViewById(R.id.MealOptionButton);
+        HistoryButton = (Button)findViewById(R.id.HistoryButton);
+        AddMealButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddMealActivity.class);
+                startActivity(intent);
+            }
+        });
+        CreateMealButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateMealActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    AddMealButton = (Button) findViewById(R.id.AddMealButton);
-    AddMealButton.setOnClickListener(new View.OnClickListener() {
-        @Override
+    }
 
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, AddMealActivity.class);
-            startActivity(intent);
-        }
-    });
-
-}}
-
-
-
+}
