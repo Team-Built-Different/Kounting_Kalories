c18de659250655daa4642a6c63f57f4521465e8e
jorgeriiios <71609360+jorgeriiios@users.noreply.github.com>
2020-11-16 18:28:18 -0800

    Update AddMealActivity.java

--- AddMealActivity.java
+++ AddMealActivity.java
@@ -1,5 +1,6 @@
+// This file was made by Jorge Rios
 package com.example.kv1;
-
+// These are the packages that are required for the application to run
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,12 +15,12 @@ public class AddMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
-        NewMealButton = (Button)findViewById(R.id.NewMealButton);
-        NewMealButton.setOnClickListener(new View.OnClickListener() {
+        NewMealButton = (Button)findViewById(R.id.NewMealButton); // Identifies NewMealButton 
+        NewMealButton.setOnClickListener(new View.OnClickListener() { // Notifys the system of user input 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AddMealActivity.this, NewMealActivity.class);
-                startActivity(intent);
+                startActivity(intent); // Allows for a new activty to start 
             }
         });
 
