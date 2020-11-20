ece72dbe39cc7b807d308ef2c77f4aed91d37699
EGonzalez3604 <71609002+EGonzalez3604@users.noreply.github.com>
2020-11-19 17:04:54 -0800

    Update NewMealActivity.java

--- NewMealActivity.java
+++ NewMealActivity.java
@@ -1,15 +1,42 @@
-//this class is done by Eric Gonzalez. this is the class for the activity_new_meal.xml file
+//this class is done by Eric Gonzalez. this is the class for the activity_new_meal.xml file variables are string Mealname and int Calories.
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class NewMealActivity extends AppCompatActivity {
+    String Mealname;
+    int Calories;
 
+    EditText MealNameInput;
+    EditText UserCaloriesInput;
+    Button IncreaseCalorieCountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meal);
+        MealNameInput =(EditText) findViewById(R.id.MealNameInput);
+        UserCaloriesInput  =(EditText) findViewById(R.id.UserCaloriesInput);
+        IncreaseCalorieCountButton = (Button)findViewById(R.id.IncreaseCalorieCountButton);
+                    IncreaseCalorieCountButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Mealname = MealNameInput.getText().toString();
+                    Calories = Integer.valueOf(UserCaloriesInput.getText().toString());
+                    Log.d("info",Mealname); //keep track in terminal with info filter
+                    Log.d("info", String.valueOf(Calories));//keep track in terminal with info filter
+
+
+
+                }
+            });
+
     }
 }
