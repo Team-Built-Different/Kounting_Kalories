5e95d98f8983e37804a6dac3fab14311ef076feb
IsaacZavala2000 <71609049+IsaacZavala2000@users.noreply.github.com>
2020-11-19 18:34:57 -0800

    Update CreateMealActivity.java

--- CreateMealActivity.java
+++ CreateMealActivity.java
@@ -1,23 +1,40 @@
-package com.example.kountingkalories; //Author: Isaac Zavala
+package com.example.kountingkalories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateMealActivity extends AppCompatActivity {
-    private Button save_food;// the button to save the food
+    String Mealname;
+    int Calories;
+    Button save_food;
+    EditText edit_cal;
+    EditText edit_meal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meal);
-        save_food = (Button) findViewById(R.id.save_food);
-        save_food.setOnClickListener(new View.OnClickListener() {       //This is where the program responds to the click of the button. 
+        edit_meal =(EditText) findViewById(R.id.edit_meal);
+        edit_cal  =(EditText) findViewById(R.id.edit_cal);
+        save_food = (Button)findViewById(R.id.save_food);
+        save_food.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {  //this is the action that is taken when the button is clicked. 
+            public void onClick(View v) {
+                Mealname = edit_meal.getText().toString();
+                Calories = Integer.valueOf(edit_cal.getText().toString());
+                Log.d("info",Mealname);
+                Log.d("info", String.valueOf(Calories));
 
-            } // This is where we need to connect our database so we can create a place where the meals the user inputs are saved.
+
+
+            }
         });
+
+
     }
 }
