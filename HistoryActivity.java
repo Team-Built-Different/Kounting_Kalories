955f3ad0788d1fc01aee32b2d1fc8846b0dcb217
jorgeriiios <71609360+jorgeriiios@users.noreply.github.com>
2020-12-08 21:31:54 -0800

    Update HistoryActivity.java

--- HistoryActivity.java
+++ HistoryActivity.java
@@ -2,7 +2,8 @@
 package com.example.kv1;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,54 +12,60 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 public class HistoryActivity extends AppCompatActivity {
-    Vector<String> VecMeal = new Vector<String>();
-    Vector<Integer> VecCalories= new Vector<Integer>();
-    //public ArrayList<String> NameOfMeal= new ArrayList<String>();
-   /// public ArrayList<int> Calories = new ArrayList<int>();
+    public ArrayList<String> dataList = new ArrayList<String>();
     public String[] myArray = {};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_history);
 
 
 
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_history);
+       findObjects();
+   }
 
-        // Configure Query
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("History");
+       private void findObjects(){
+           myArray = new String[]{};
+           final ListView listView = (ListView) findViewById(R.id.listviewA);
 
-        // Query Parameters
-        query.whereExists("Calories");
+           // Configure Query
+           ParseQuery<ParseObject> query = ParseQuery.getQuery("History");
 
-        // Sorts the results in ascending order by the itemName field
-        query.orderByAscending("Calories");
+           // Query Parameters
+           query.whereExists("Calories");
 
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, final ParseException e) {
-                if (e == null){
-                    // Adding objects into the Array
-                    for(int i= 0 ; i < objects.size(); i++){
-                        String element = objects.get(i).getString("NameOfMeal");
-                        int temp = objects.get(i).getInt("Calories");
+           // Sorts the results in ascending order by the itemName field
+           query.orderByAscending("Calories");
 
-                        VecMeal.add(element.toString());
-                        VecCalories.add(temp);
-                        Log.d("info", String.valueOf(VecMeal));
-                        Log.d("info", String.valueOf(VecCalories));
-                    }
-                } else {
+           query.findInBackground(new FindCallback<ParseObject>() {
+               @Override
+               public void done(List<ParseObject> objects, final ParseException e) {
+                   if (e == null) {
+                       // Adding objects into the Array
+                       for (int i = 0; i < objects.size(); i++) {
+                           String element = objects.get(i).getString("NameOfMeal");
+                           
+                           dataList.add(element.toString());
+                       }
+                   } else {
 
-                }
-               // myArray = dataList.toArray(new String[dataList.size()]);
+                   }
+                   myArray = dataList.toArray(new String[dataList.size()]);
+
+                   final ArrayList<String> list = new ArrayList<String>(Arrays.asList(myArray));
+
+                   ArrayAdapter<String> adapterList = new ArrayAdapter<String>(HistoryActivity.this, android.R.layout.simple_list_item_single_choice, myArray);
 
 
-    }
-});
-    }
+                   listView.setAdapter(adapterList);
+
+               }});}
+
+
+
 }
