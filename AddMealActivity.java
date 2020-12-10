f9ba7c78904b3deedc4df7f866e0ccb238878380
jorgeriiios <71609360+jorgeriiios@users.noreply.github.com>
2020-12-09 18:56:08 -0800

    Update AddMealActivity.java

--- AddMealActivity.java
+++ AddMealActivity.java
@@ -1,26 +1,35 @@
 // This file was made by Jorge Rios
 package com.example.kv1;
 // These are the packages that are required for the application to run
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class AddMealActivity extends AppCompatActivity {
     Button NewMealButton;
+    Button CustomMealsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
-        NewMealButton = (Button)findViewById(R.id.NewMealButton); // Identifies NewMealButton 
-        NewMealButton.setOnClickListener(new View.OnClickListener() { // Notifys the system of user input 
+        CustomMealsButton = (Button)findViewById(R.id.CustomMealsButton);
+        NewMealButton = (Button)findViewById(R.id.NewMealButton);
+        NewMealButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AddMealActivity.this, NewMealActivity.class);
-                startActivity(intent); // Allows for a new activty to start 
+                startActivity(intent);
+            }
+        });
+
+        CustomMealsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddMealActivity.this, CustomMealsActivity.class);
+                startActivity(intent);
             }
         });
 
